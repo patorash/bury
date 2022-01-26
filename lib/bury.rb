786910b8728cc422cadce1e0ef76c4b371e4a2a0
patorash <chariderpato@gmail.com>
2022-01-26 19:10:43 +0900

    refactor: Add lambda function instead of private method.

--- lib/bury.rb
+++ lib/bury.rb
@@ -11,31 +11,26 @@ module Bury
   #   {}.bury([:a,:b,:c], 1) # => {a:{b:{c: 1}}}
   #   {a: {d: 2}}.bury([:a,:b,:c], 1) # => {a:{b:{c:1},d:2}}
   def bury(keys, value)
-    merge_for_burying = lambda do |hash, keys, target_hash, index = 0|
-      key = keys[index]
+    merge_for_burying = lambda do |hash, _keys, target_hash, index = 0|
+      key = _keys[index]
       if hash.has_key?(key) && hash[key].is_a?(Hash)
-        merge_for_burying.call(hash[key], keys, target_hash, index + 1)
+        merge_for_burying.call(hash[key], _keys, target_hash, index + 1)
       else
-        hash.merge!(index.zero? ? target_hash : target_hash.dig(*keys.take(index)))
+        hash.merge!(index.zero? ? target_hash : target_hash.dig(*_keys.take(index)))
       end
     end
-    merge_for_burying.call(self, keys, create_hash_recursively(keys, value))
+
+    create_hash_recursively = lambda do |_keys, _value, index = 0|
+      if _keys.size - 1 == index
+        { _keys[index] => _value }
+      else
+        { _keys[index] => create_hash_recursively.call(_keys, _value, index + 1)}
+      end
+    end
+
+    merge_for_burying.call(self, keys, create_hash_recursively.call(keys, value))
 
     self
-  end
-
-  private
-
-  # @param [Array<String|Symbol>] keys
-  # @param [Object] value
-  # @option [Integer] index
-  # @return [Hash]
-  def create_hash_recursively(keys, value, index = 0)
-    if keys.size - 1 == index
-      { keys[index] => value }
-    else
-      { keys[index] => create_hash_recursively(keys, value, index + 1) }
-    end
   end
 
 end
