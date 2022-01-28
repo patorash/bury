df28335809a7db8a4f61e641332e77f76b01ebd9
patorash <chariderpato@gmail.com>
2022-01-28 13:26:21 +0900

    test: Added tests for destructive method.

--- test/bury_test.rb
+++ test/bury_test.rb
@@ -6,36 +6,86 @@ class BuryTest < Minitest::Test
   end
 
   def test_it_get_simple_hash
-    assert_equal({a:{b:{c: 1}}}, {}.bury([:a, :b, :c], 1))
+    hash = {}
+    assert_equal({a:{b:{c: 1}}}, hash.bury([:a, :b, :c], 1))
+    assert_equal({}, hash)
   end
 
   def test_it_get_complexed_hash
     hash = {a: {d: 2}}
     assert_equal({a:{d: 2,b:{c: 1}}}, hash.bury([:a,:b,:c],1))
+    assert_equal({a: {d: 2}}, hash)
   end
 
   def test_it_includes_same_key
     hash = {a: {a: 2}}
     assert_equal({a:{a: 2,b:{c: 1}}}, hash.bury([:a,:b,:c],1))
+    assert_equal({a: {a: 2}}, hash)
   end
 
   def test_it_have_multiple_keys
     hash = {a: {d: 2}, e: 1}
     assert_equal({a:{d: 2,b:{c: 1}}, e: 1}, hash.bury([:a,:b,:c],1))
+    assert_equal({a: {d: 2}, e: 1}, hash)
   end
 
   def test_it_have_multiple_keys2
     hash = {e: 1}
     assert_equal({a:{b:{c: 1}}, e: 1}, hash.bury([:a,:b,:c],1))
+    assert_equal({e: 1}, hash)
   end
 
   def test_it_have_multiple_keys3
     hash = {b: {a: {c: 1}}}
     assert_equal({a:{b:{c: 1}}, b:{a: {c: 1}}}, hash.bury([:a,:b,:c],1))
+    assert_equal({b: {a: {c: 1}}}, hash)
   end
 
   def test_it_have_multiple_keys4
     hash = {a: {b: {d: 1}}}
     assert_equal({a:{b:{c: 1, d: 1}}}, hash.bury([:a,:b,:c],1))
+    assert_equal({a: {b: {d: 1}}}, hash)
+  end
+
+  def test_it_get_simple_hash!
+    hash = {}
+    assert_equal({a:{b:{c: 1}}}, hash.bury!([:a, :b, :c], 1))
+    assert_equal({a:{b:{c: 1}}}, hash)
+  end
+
+  def test_it_get_complexed_hash!
+    hash = {a: {d: 2}}
+    assert_equal({a:{d: 2,b:{c: 1}}}, hash.bury!([:a,:b,:c],1))
+    assert_equal({a:{d: 2,b:{c: 1}}}, hash)
+  end
+
+  def test_it_includes_same_key!
+    hash = {a: {a: 2}}
+    assert_equal({a:{a: 2,b:{c: 1}}}, hash.bury!([:a,:b,:c],1))
+    assert_equal({a:{a: 2,b:{c: 1}}}, hash)
+  end
+
+  def test_it_have_multiple_keys!
+    hash = {a: {d: 2}, e: 1}
+    assert_equal({a:{d: 2,b:{c: 1}}, e: 1}, hash.bury!([:a,:b,:c],1))
+    assert_equal({a:{d: 2,b:{c: 1}}, e: 1}, hash)
+  end
+
+  def test_it_have_multiple_keys2!
+    hash = {e: 1}
+    assert_equal({a:{b:{c: 1}}, e: 1}, hash.bury!([:a,:b,:c],1))
+    assert_equal({a:{b:{c: 1}}, e: 1}, hash)
+  end
+
+  def test_it_have_multiple_keys3!
+    hash = {b: {a: {c: 1}}}
+    assert_equal({a:{b:{c: 1}}, b:{a: {c: 1}}}, hash.bury!([:a,:b,:c],1))
+    assert_equal({a:{b:{c: 1}}, b:{a: {c: 1}}}, hash)
+  end
+
+  def test_it_have_multiple_keys4!
+    hash = {a: {b: {d: 1}}}
+    assert_equal({a:{b:{c: 1, d: 1}}}, hash.bury!([:a,:b,:c],1))
+    assert_equal({a:{b:{c: 1, d: 1}}}, hash)
   end
 end
